5c1e5e925ce10833243d01c12f907bc9bc5b9b33
edwincarbajal <exaviercarbajal@gmail.com>
2018-04-18 19:26:47 -0400

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
 5.times do
   Framework.create!(
     title: Faker::Lorem.word,
@@ -19,7 +18,7 @@ end
     title: Faker::Lorem.word,
     description: Faker::Lorem.paragraph,
     website: Faker::Internet.url,
-    author: Faker::Lorem.word,
+    author: 'John Doe',
     framework: Framework.all.sample,
     skill_level: Faker::Lorem.word
   )
