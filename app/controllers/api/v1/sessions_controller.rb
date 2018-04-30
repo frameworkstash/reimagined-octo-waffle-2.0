74d2891ddd2445dfbcb8041e00a6210aae6bb417
edwincarbajal <exaviercarbajal@gmail.com>
2018-04-30 06:08:25 -0400

    Create sessions controller and helper

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    @user = User.where(uid: auth_params[:uid]).first_or_create! do |user|
+      user.displayName = auth_params[:displayName]
+      user.email = auth_params[:email]
+      user.providerId = auth_params[:providerId]
+      user.photoURL = auth_params[:photoURL]
+    end
+    if @user
+      @jwt = Auth.issue({ user: @user.id })
+      render '/api/v1/users/show', locals: { user: @user, jwt: @jwt }
+    end
+  end
+
+  private
+    def auth_params
+      params.require(:auth).permit(:displayName, :email, :providerId, :photoURL, :uid)
+    end
+end
