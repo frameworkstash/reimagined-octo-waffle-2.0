fd280301c43c7a40babd99876627cf81dc26ddec
edwincarbajal <exaviercarbajal@gmail.com>
2018-06-05 14:16:54 -0400

    Add helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_request
+  helper_method :signed_in?, :current_user
+  attr_reader :current_user
+
+  private
+    def authenticate_request
+      return if request.headers['Authorization'].blank?
+      @current_user = AuthorizeApiRequest.call(request.headers).result
+      render json: { error: 'Not Authorized' }, status: 401 unless @current_user
+    end
+
+    def current_user
+      @current_user ||= super || User.find(@current_user.id)
+    end
+
+    def signed_in?
+      @current_user.present?
+    end
 end
