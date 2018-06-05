84600f694abaa06c99e606883c79d878960eb806
edwincarbajal <exaviercarbajal@gmail.com>
2018-06-05 14:18:34 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,16 @@ Rails.application.routes.draw do
   mount ForestLiana::Engine => '/forest'
   scope module: 'api', defaults: { format: 'json' } do
     namespace :v1 do
-      post '/login', to: 'sessions#create'
-      get '/users/:id', to: 'users#show'
-      resources :users, only: [:update, :destroy]
+      post '/auth/login', to: 'authentication#authenticate'
+      # resources :user, only: [:show, :update, :destroy]
+      get '/user', to: 'users#show'
+      resources :profiles, param: :username, only: [:show]
       resources :frameworks do
         resources :tutorials, only: [:create, :update]
       end
-      resources :tutorials, except: [:create, :update] do
+      resources :tutorials, param: :slug, except: [:edit, :new] do
         resources :comments, only: [:create, :update, :destroy]
-        # resources :likes, only: [:create, :destroy]
+        resources :likes, only: [:create, :destroy]
         member do
           post 'upvote'
         end
