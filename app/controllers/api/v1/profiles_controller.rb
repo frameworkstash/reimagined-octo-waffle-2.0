bf78d2bff6893cb5227a57a6ec31357b89a5482e
edwincarbajal <exaviercarbajal@gmail.com>
2018-08-28 17:59:54 -0400

    Create a seperate route for user activity

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,7 +1,18 @@
 class Api::V1::ProfilesController < ApplicationController
   before_action :authenticate_request
+  skip_before_action :authenticate_request, only: [:activity]
 
   def show
     @user = User.find_by_username(params[:username])
+  end
+
+  def activity
+    @tutorials = Tutorial.all
+    @tutorials = @tutorials.upvoted_by(params[:upvoted]) if params[:upvoted].present?
+    @tutorials = @tutorials.submitted_by(params[:submitted]) if params[:submitted].present?
+
+    @tutorials = @tutorials.order('created_at DESC')
+
+    render '/api/v1/profiles/activity', locals: { tutorial: @tutorials }
   end
 end
